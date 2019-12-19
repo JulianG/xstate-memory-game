3eff7b312688c33c4c837904a1bda46a7598a415
JulianG <juliangaramendy@gmail.com>
2019-12-19 09:02:54 +0000

    fixed footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-  
+
 export const Footer = () => (
   <footer>
-    Icons made by
+    Icons made by{' '}
     <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
       Freepik
-    </a>
-    from
+    </a>{' '}
+    from{' '}
     <a href="https://www.flaticon.com/" title="Flaticon">
       www.flaticon.com
     </a>
