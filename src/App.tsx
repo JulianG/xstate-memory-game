b95598c27e72554025ec7e71854796f7a0d34c62
JulianG <juliangaramendy@gmail.com>
2019-12-23 12:28:44 +0000

    a bit more responsive

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import { createMemoryGameMachine, GameCard } from './memory-game'
 import { CardGrid, Card, CollectedCard, CardBack, NoCard } from './cards/Card'
 import { Footer } from './Footer'
 
+const numCards = Number.parseInt(window.location.pathname.substr(1)) || 12
+
 const machine = createMemoryGameMachine({
-  cards: createCards(6),
+  cards: createCards(Math.floor(numCards / 2)),
   pairs: [],
   firstSelected: null,
   secondSelected: null
@@ -29,10 +31,12 @@ const App: React.FC = () => {
     }
   }, [current.value, send])
 
+  const scale = 20 / current.context.cards.length
+
   return current.matches('finished') ? (
     <CardGrid>
       {current.context.cards.map((card, index) => (
-        <CollectedCard key={index} cardId={card.type} />
+        <CollectedCard key={index} cardId={card.type} scale={scale} />
       ))}
     </CardGrid>
   ) : (
@@ -42,12 +46,16 @@ const App: React.FC = () => {
           const visible = card === firstSelected || card === secondSelected
           return !card.collected ? (
             visible ? (
-              <Card key={index} cardId={card.type} />
+              <Card key={index} cardId={card.type} scale={scale} />
             ) : (
-              <CardBack key={index} onSelect={() => selectCard(card)} />
+              <CardBack
+                key={index}
+                onSelect={() => selectCard(card)}
+                scale={scale}
+              />
             )
           ) : (
-            <NoCard key={index} />
+            <NoCard key={index} scale={scale} />
           )
         })}
       </CardGrid>
--- src/cards/Card.tsx
+++ src/cards/Card.tsx
@@ -26,27 +26,73 @@ const cards = [
 ]
 
 export const CardGrid: React.FC = ({ children }) => {
-  return <div className="cardgrid">{children}</div>
+  // const numCards = Array.isArray(children) ? children.length : 1
+
+  // const cols = Math.floor(Math.sqrt(numCards))
+  // const rows = Array.isArray(children) ? Math.ceil(children.length / cols) : 1
+
+  // const min = Math.max(rows, cols)
+
+  // const cellSize = `${90 / min}vmin`
+
+  // console.log('rows', rows)
+  // console.log('min', min)
+  // console.log('cellSize', cellSize)
+  const style = {
+    // marginLeft: `1vmin`,
+    // marginTop: `1vmin`,
+    // gridTemplateColumns: `repeat(${cols}, ${cellSize})`,
+    // gridTemplateRows: `repeat(${rows}, ${cellSize})`,
+    // gridColumnGap: `1vmin`,
+    // gridRowGap: `1vmin`,
+    // width: `100%`
+  }
+
+  return (
+    <div className="cardgrid" style={style}>
+      {children}
+    </div>
+  )
 }
 
 type VisibleCardProps = {
   cardId: number
+  scale: number
 }
-export const Card: React.FC<VisibleCardProps> = ({ cardId }) => {
+
+const getCardImgStyle = (scale: number) => ({
+  width: `${15 * scale}vmin`,
+  height: `${15 * scale}vmin`
+})
+
+export const Card: React.FC<VisibleCardProps> = ({ cardId, scale }) => {
   return (
     <div className="card">
       <div className="cardbg">
-        <img alt={`card-${cardId}`} src={cards[cardId]} draggable={false} />
+        <img
+          alt={`card-${cardId}`}
+          src={cards[cardId]}
+          style={getCardImgStyle(scale)}
+          draggable={false}
+        />
       </div>
     </div>
   )
 }
 
-export const CollectedCard: React.FC<VisibleCardProps> = ({ cardId }) => {
+export const CollectedCard: React.FC<VisibleCardProps> = ({
+  cardId,
+  scale
+}) => {
   return (
     <div className="card collected">
       <div className="cardbg">
-        <img alt={`card-${cardId}`} src={cards[cardId]} draggable={false} />
+        <img
+          alt={`card-${cardId}`}
+          src={cards[cardId]}
+          style={getCardImgStyle(scale)}
+          draggable={false}
+        />
       </div>
     </div>
   )
@@ -54,21 +100,32 @@ export const CollectedCard: React.FC<VisibleCardProps> = ({ cardId }) => {
 
 type CardBackProps = {
   onSelect: () => void
+  scale: number
 }
 
-export const CardBack: React.FC<CardBackProps> = ({ onSelect }) => (
+export const CardBack: React.FC<CardBackProps> = ({ onSelect, scale }) => (
   <div className="card" onClick={onSelect}>
     <div className="cardbg">
-      <img alt={`card-back`} src={cards[0]} draggable={false} />
+      <img
+        alt={`card-back`}
+        src={cards[0]}
+        style={getCardImgStyle(scale)}
+        draggable={false}
+      />
     </div>
   </div>
 )
 
-export const NoCard = () => {
+export const NoCard = ({ scale }: { scale: number }) => {
   return (
     <div className="card nocard">
       <div className="cardbg nocard">
-        <img alt={`card-no-card`} src={cards[0]} draggable={false} />
+        <img
+          alt={`card-no-card`}
+          src={cards[0]}
+          style={getCardImgStyle(scale)}
+          draggable={false}
+        />
       </div>
     </div>
   )
