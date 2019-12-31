1b41e655f136b4ab153a9665ac84e5f3debac5c4
JulianG <juliangaramendy@gmail.com>
2019-12-31 18:32:15 +0000

    fixed test

--- src/memory-game.test.ts
+++ src/memory-game.test.ts
@@ -3,7 +3,7 @@ import { interpret } from 'xstate'
 import { createCards } from './factory'
 
 describe('memory-game', () => {
-  test('selecting 2 cards', () => {
+  test('happy path', () => {
     const machine = createMemoryGameMachine({
       cards: createCards(3),
       pairs: [],
@@ -13,18 +13,110 @@ describe('memory-game', () => {
 
     const service = interpret(machine).start()
 
-    service.onTransition(state => console.log('state', state.value))
+    const getContext = () => service.state.context
+    const assertState = (s: string) => {
+      expect(service.state.matches(s)).toBe(true)
+    }
 
     service.send({ type: 'SELECT', index: 0 })
 
-    expect(service.state.value).toEqual('oneSelected')
-    expect(service.machine.context!.firstSelected) //
-      .toMatchObject({ type: 1 }) // ok
+    assertState('oneSelected')
+    expect(getContext().firstSelected).toMatchObject({ type: 1 })
 
     service.send({ type: 'SELECT', index: 1 })
 
-    expect(service.state.value).toEqual('twoSelected')
-    expect(service.machine.context!.secondSelected) //
-      .toMatchObject({ type: 1 }) // fails here. apparently secondSelected is null
+    assertState('twoSelected')
+    expect(getContext().firstSelected).toMatchObject({ type: 1 })
+    expect(getContext().secondSelected).toMatchObject({ type: 1 })
+
+    service.send('CONTINUE')
+
+    assertState('idle')
+    expect(getContext().firstSelected).toBeNull()
+    expect(getContext().secondSelected).toBeNull()
+    expect(getContext().pairs).toMatchObject([{ type: 1 }, { type: 1 }])
+    expect(getContext().cards).toMatchObject([
+      { type: 1, collected: true },
+      { type: 1, collected: true },
+      { type: 2 },
+      { type: 2 },
+      { type: 3 },
+      { type: 3 }
+    ])
+
+    service.send({ type: 'SELECT', index: 2 })
+    expect(getContext().firstSelected).toMatchObject({ type: 2 })
+
+    service.send({ type: 'SELECT', index: 4 })
+    expect(getContext().secondSelected).toMatchObject({ type: 3 })
+
+    service.send({ type: 'CONTINUE' })
+
+    assertState('idle')
+    expect(getContext().firstSelected).toBeNull()
+    expect(getContext().secondSelected).toBeNull()
+    expect(getContext().pairs).toMatchObject([{ type: 1 }, { type: 1 }])
+    expect(getContext().cards).toMatchObject([
+      { type: 1, collected: true },
+      { type: 1, collected: true },
+      { type: 2 },
+      { type: 2 },
+      { type: 3 },
+      { type: 3 }
+    ])
+
+    service.send({ type: 'SELECT', index: 2 })
+    service.send({ type: 'SELECT', index: 3 })
+
+    expect(getContext().firstSelected).toMatchObject({ type: 2 })
+    expect(getContext().firstSelected).toMatchObject({ type: 2 })
+
+    service.send({ type: 'CONTINUE' })
+
+    assertState('idle')
+    expect(getContext().firstSelected).toBeNull()
+    expect(getContext().secondSelected).toBeNull()
+    expect(getContext().pairs).toMatchObject([
+      { type: 1 },
+      { type: 1 },
+      { type: 2 },
+      { type: 2 }
+    ])
+    expect(getContext().cards).toMatchObject([
+      { type: 1, collected: true },
+      { type: 1, collected: true },
+      { type: 2, collected: true },
+      { type: 2, collected: true },
+      { type: 3 },
+      { type: 3 }
+    ])
+    
+    service.send({ type: 'SELECT', index: 4 })    
+    service.send({ type: 'SELECT', index: 5 })
+
+    expect(getContext().firstSelected).toMatchObject({ type: 3 })
+    expect(getContext().secondSelected).toMatchObject({ type: 3 })
+
+    service.send({ type: 'CONTINUE' })
+
+    assertState('idle')
+    expect(getContext().pairs).toMatchObject([
+      { type: 1 },
+      { type: 1 },
+      { type: 2 },
+      { type: 2 },
+      { type: 3 },
+      { type: 3 }
+    ])
+    expect(getContext().cards).toMatchObject([
+      { type: 1, collected: true },
+      { type: 1, collected: true },
+      { type: 2, collected: true },
+      { type: 2, collected: true },
+      { type: 3, collected: true },
+      { type: 3, collected: true }
+    ])
+
+    assertState('finished')
   })
 })
